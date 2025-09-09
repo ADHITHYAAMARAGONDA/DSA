39013a0838b0be6ca978cef99b7531580c974ae2
ADHIYHYAAMARAGONDA <119164936+ADHITHYAAMARAGONDA@users.noreply.github.com>
2025-09-09 18:41:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (50.00%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    // public int fib(int n) {
+        
+    //     if(n<=1){
+    //         return n;
+    //     }
+
+    //     int res=fib(n-1)+fib(n-2);
+    //     return res;
+    // }
+    public int fib(int n) {
+int[]dp=new int[n+1];
+Arrays.fill(dp,-1);
+return helper(n,dp);
+    }
+    private int helper(int n,int[]dp){
+if(n<=1)return n;
+ if(dp[n]!=-1) return dp[n];
+return dp[n]=helper(n-1,dp)+helper(n-2,dp);
+}
+}
